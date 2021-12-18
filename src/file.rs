777138639e49f6ff786b8d6a886745c5bf1afee9
Michaellu <mchblch@protonmail.com>
2021-12-18 20:06:41 +0100

    changed theme design

--- src/file.rs
+++ src/file.rs
@@ -14,19 +14,19 @@ use std::path::Path;
 #[derive(Debug)]
 pub struct Pattern
 {
-	pub dim: (u32, u32),
+	pub dim: u32,
 	pub colors: Vec<Color>,
-	pub rot: Vec<Bitmap<64>>,
+	pub template: Bitmap<64>,
 }
 
 impl Pattern
 {
-	fn new(dim: (u32, u32), rot: Vec<Bitmap<64>>) -> Self
+	fn new(dim: u32, template: Bitmap<64>) -> Self
 	{
 		Pattern {
 			dim,
 			colors: vec![Color::RED; 4],
-			rot,
+			template,
 		}
 	}
 }
@@ -187,75 +187,62 @@ fn parse_piece(r: &mut Reader<BufReader<File>>, e: &BytesStart) -> Result<Patter
 	Ok(Pattern::new(dim, rots))
 }
 
-fn parse_piece_size(
-	r: &mut Reader<BufReader<File>>,
-	e: &BytesStart,
-) -> Result<(u32, u32), ParseError>
+fn parse_piece_size(r: &mut Reader<BufReader<File>>, e: &BytesStart) -> Result<u32, ParseError>
 {
 	let mut w = 0;
-	let mut h = 0;
 
 	for attrib in e.attributes() {
 		let a = attrib?;
 		let v = a.unescape_and_decode_value(r)?;
 
 		match a.key {
-			b"w" => w = v.parse()?,
-			b"h" => h = v.parse()?,
+			b"r" => w = v.parse()?,
 			_ => (),
 		}
 	}
 
 	if w == 0 {
-		return Err(ParseError::from_str("A piece has a missing width."));
+		return Err(ParseError::from_str(
+			"Pieces can't have a 0 width or a piece has a missing width.",
+		));
 	}
 
-	if h == 0 {
-		return Err(ParseError::from_str("A piece has a missing height."));
-	}
-
-	if w * h > 64 {
+	if w * w > 64 {
 		return Err(ParseError::from_str("A piece size exceeds 64 slots."));
 	}
 
-	Ok((w, h))
+	Ok(w)
 }
 
 fn parse_template(
 	r: &mut Reader<BufReader<File>>,
-	s: (u32, u32),
+	s: u32,
 	t: &BytesText,
-) -> Result<Vec<Bitmap<64>>, ParseError>
+) -> Result<Bitmap<64>, ParseError>
 {
 	let mut data = t.unescape_and_decode(r)?;
 	data.retain(|c| !c.is_whitespace());
 
-	let mut rotations: Vec<_> = Vec::new();
-
-	for rot in data.split('#') {
-		if s.0 * s.1 != rot.len() as u32 {
-			return Err(ParseError::from_str(
-				format!(
-					"A piece size doesn't match it's given size. is: {}, should be: {}.",
-					data.len().to_string(),
-					(s.0 * s.1).to_string()
-				)
-				.as_str(),
-			));
-		}
-
-		let mut field = Bitmap::<64>::new();
-
-		for i in rot.char_indices() {
-			match i.1 {
-				'1' => drop(field.set(i.0, true)),
-				'0' => (),
-				_ => return Err(ParseError::from_str("Characters must be 0's or 1's.")),
-			}
-		}
-
-		rotations.push(field);
+	if s * s != data.len() as u32 {
+		return Err(ParseError::from_str(
+			format!(
+				"A piece size doesn't match it's given size. is: {}, should be: {}.",
+				data.len().to_string(),
+				(s * s).to_string()
+			)
+			.as_str(),
+		));
 	}
 
-	Ok(rotations)
+	let mut field = Bitmap::<64>::new();
+
+	for i in data.char_indices() {
+		match i.1 {
+			'1' => drop(field.set(i.0, true)),
+			'0' => (),
+			_ => return Err(ParseError::from_str("Characters must be 0's or 1's.")),
+		}
+	}
+
+	Ok(field)
 }
