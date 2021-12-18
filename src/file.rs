0b8bbfd4a1c43e88037b0e0c0254984cfda73eba
Michaellu <mchblch@protonmail.com>
2021-12-18 16:21:36 +0100

    Sorted file.rs

--- src/file.rs
+++ src/file.rs
@@ -42,44 +42,6 @@ pub struct Theme
 	pub patterns: Vec<Pattern>,
 }
 
-// -----------------------------------------------------------------------------
-// Parser
-// -----------------------------------------------------------------------------
-
-struct ParseError
-{
-	err: String,
-}
-
-impl ParseError
-{
-	fn from_str(s: &str) -> Self
-	{
-		ParseError { err: s.to_string() }
-	}
-
-	fn from_pos(r: &Reader<BufReader<File>>, e: &Error) -> Self
-	{
-		ParseError::from_str(format!("Error at position {}: {:?}", r.buffer_position(), e).as_str())
-	}
-}
-
-impl From<Error> for ParseError
-{
-	fn from(e: Error) -> Self
-	{
-		ParseError { err: e.to_string() }
-	}
-}
-
-impl From<ParseIntError> for ParseError
-{
-	fn from(e: ParseIntError) -> Self
-	{
-		ParseError { err: e.to_string() }
-	}
-}
-
 impl Theme
 {
 	fn new() -> Theme
@@ -116,7 +78,52 @@ impl Theme
 			patterns,
 		}
 	}
+}
 
+// -----------------------------------------------------------------------------
+// Error
+// -----------------------------------------------------------------------------
+
+struct ParseError
+{
+	err: String,
+}
+
+impl ParseError
+{
+	fn from_str(s: &str) -> Self
+	{
+		ParseError { err: s.to_string() }
+	}
+
+	fn from_pos(r: &Reader<BufReader<File>>, e: &Error) -> Self
+	{
+		ParseError::from_str(format!("Error at position {}: {:?}", r.buffer_position(), e).as_str())
+	}
+}
+
+impl From<Error> for ParseError
+{
+	fn from(e: Error) -> Self
+	{
+		ParseError { err: e.to_string() }
+	}
+}
+
+impl From<ParseIntError> for ParseError
+{
+	fn from(e: ParseIntError) -> Self
+	{
+		ParseError { err: e.to_string() }
+	}
+}
+
+// -----------------------------------------------------------------------------
+// Theme Parser
+// -----------------------------------------------------------------------------
+
+impl Theme
+{
 	pub fn load(path: &Path) -> Option<Theme>
 	{
 		let mut reader = if let Ok(reader) = Reader::from_file(path) {
@@ -130,7 +137,7 @@ impl Theme
 			return None;
 		};
 
-		match Theme::parse_tetris(&mut reader) {
+		match parse_tetris(&mut reader) {
 			Ok(t) => Some(t),
 			Err(e) => {
 				println!("Theme couldn't be parsed. {}", e.err);
@@ -138,124 +145,117 @@ impl Theme
 			}
 		}
 	}
-
-	fn parse_tetris(r: &mut Reader<BufReader<File>>) -> Result<Theme, ParseError>
-	{
-		let mut patterns: Vec<_> = Vec::new();
-		let mut buf = Vec::new();
-
-		loop {
-			match r.read_event(&mut buf) {
-				Ok(Event::Eof) => break,
-				Ok(Event::Start(ref e)) => {
-					if let b"piece" = e.name() {
-						patterns.push(Theme::parse_piece(r, e)?);
-					}
-				}
-
-				Err(e) => return Err(ParseError::from_pos(r, &e)),
-				_ => (),
-			}
-
-			buf.clear();
-		}
-
-		Ok(Theme::from_patterns(patterns))
-	}
-
-	fn parse_piece(r: &mut Reader<BufReader<File>>, e: &BytesStart) -> Result<Pattern, ParseError>
-	{
-		let dim = Theme::parse_piece_size(r, &e)?;
-
-		let mut buf = Vec::new();
-
-		let rots = match r.read_event(&mut buf) {
-			Ok(Event::Text(e)) => Theme::parse_template(r, dim, &e)?,
-
-			Err(e) => return Err(ParseError::from_pos(r, &e)),
-			_ => return Err(ParseError::from_str("Missing piece rotations")),
-		};
-
-		Ok(Pattern::new(dim, rots))
-	}
 }
 
-// -----------------------------------------------------------------------------
-// Piece Parser
-// -----------------------------------------------------------------------------
-
-impl Theme
+fn parse_tetris(r: &mut Reader<BufReader<File>>) -> Result<Theme, ParseError>
 {
-	fn parse_piece_size(
-		r: &mut Reader<BufReader<File>>,
-		e: &BytesStart,
-	) -> Result<(u32, u32), ParseError>
-	{
-		let mut w = 0;
-		let mut h = 0;
+	let mut patterns: Vec<_> = Vec::new();
+	let mut buf = Vec::new();
 
-		for attrib in e.attributes() {
-			let a = attrib?;
-			let v = a.unescape_and_decode_value(r)?;
-
-			match a.key {
-				b"w" => w = v.parse()?,
-				b"h" => h = v.parse()?,
-				_ => (),
-			}
-		}
-
-		if w == 0 {
-			return Err(ParseError::from_str("A piece has a missing width."));
-		}
-
-		if h == 0 {
-			return Err(ParseError::from_str("A piece has a missing height."));
-		}
-
-		if w * h > 64 {
-			return Err(ParseError::from_str("A piece size exceeds 64 slots."));
-		}
-
-		Ok((w, h))
-	}
-
-	fn parse_template(
-		r: &mut Reader<BufReader<File>>,
-		s: (u32, u32),
-		t: &BytesText,
-	) -> Result<Vec<Bitmap<64>>, ParseError>
-	{
-		let mut data = t.unescape_and_decode(r)?;
-		data.retain(|c| !c.is_whitespace());
-
-		let mut rotations: Vec<_> = Vec::new();
-
-		for rot in data.split('#') {
-			if s.0 * s.1 != rot.len() as u32 {
-				return Err(ParseError::from_str(
-					format!(
-						"A piece size doesn't match it's given size. is: {}, should be: {}.",
-						data.len().to_string(),
-						(s.0 * s.1).to_string()
-					)
-					.as_str(),
-				));
-			}
-
-			let mut field = Bitmap::<64>::new();
-
-			for i in rot.char_indices() {
-				match i.1 {
-					'1' => drop(field.set(i.0, true)),
-					'0' => (),
-					_ => return Err(ParseError::from_str("Characters must be 0's or 1's.")),
+	loop {
+		match r.read_event(&mut buf) {
+			Ok(Event::Eof) => break,
+			Ok(Event::Start(ref e)) => {
+				if let b"piece" = e.name() {
+					patterns.push(parse_piece(r, e)?);
 				}
 			}
 
-			rotations.push(field);
+			Err(e) => return Err(ParseError::from_pos(r, &e)),
+			_ => (),
 		}
 
-		Ok(rotations)
+		buf.clear();
 	}
+
+	Ok(Theme::from_patterns(patterns))
+}
+
+fn parse_piece(r: &mut Reader<BufReader<File>>, e: &BytesStart) -> Result<Pattern, ParseError>
+{
+	let dim = parse_piece_size(r, &e)?;
+
+	let mut buf = Vec::new();
+
+	let rots = match r.read_event(&mut buf) {
+		Ok(Event::Text(e)) => parse_template(r, dim, &e)?,
+
+		Err(e) => return Err(ParseError::from_pos(r, &e)),
+		_ => return Err(ParseError::from_str("Missing piece rotations")),
+	};
+
+	Ok(Pattern::new(dim, rots))
+}
+
+fn parse_piece_size(
+	r: &mut Reader<BufReader<File>>,
+	e: &BytesStart,
+) -> Result<(u32, u32), ParseError>
+{
+	let mut w = 0;
+	let mut h = 0;
+
+	for attrib in e.attributes() {
+		let a = attrib?;
+		let v = a.unescape_and_decode_value(r)?;
+
+		match a.key {
+			b"w" => w = v.parse()?,
+			b"h" => h = v.parse()?,
+			_ => (),
+		}
+	}
+
+	if w == 0 {
+		return Err(ParseError::from_str("A piece has a missing width."));
+	}
+
+	if h == 0 {
+		return Err(ParseError::from_str("A piece has a missing height."));
+	}
+
+	if w * h > 64 {
+		return Err(ParseError::from_str("A piece size exceeds 64 slots."));
+	}
+
+	Ok((w, h))
+}
+
+fn parse_template(
+	r: &mut Reader<BufReader<File>>,
+	s: (u32, u32),
+	t: &BytesText,
+) -> Result<Vec<Bitmap<64>>, ParseError>
+{
+	let mut data = t.unescape_and_decode(r)?;
+	data.retain(|c| !c.is_whitespace());
+
+	let mut rotations: Vec<_> = Vec::new();
+
+	for rot in data.split('#') {
+		if s.0 * s.1 != rot.len() as u32 {
+			return Err(ParseError::from_str(
+				format!(
+					"A piece size doesn't match it's given size. is: {}, should be: {}.",
+					data.len().to_string(),
+					(s.0 * s.1).to_string()
+				)
+				.as_str(),
+			));
+		}
+
+		let mut field = Bitmap::<64>::new();
+
+		for i in rot.char_indices() {
+			match i.1 {
+				'1' => drop(field.set(i.0, true)),
+				'0' => (),
+				_ => return Err(ParseError::from_str("Characters must be 0's or 1's.")),
+			}
+		}
+
+		rotations.push(field);
+	}
+
+	Ok(rotations)
 }
